e9d3d03d9d15165f5af2dad2c4583faeeb367a97
Ben shaty <benshaty@gmail.com>
2019-01-28 11:59:41 +0200

    added some delay and clear screen

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -4,6 +4,7 @@
 #include <math.h>
 #include <time.h>
 #include <conio.h>
+#include <windows.h>
 #include "source.h"
 #define _CRT_SECURE_NO_WARNINGS
 
@@ -15,12 +16,17 @@ int main() {
 	GameColor * node;
 	char userSelection;
 	srand((unsigned int) time(NULL));   // Initialization, should only be called once.
-	printf("Level 1\n");
+	printf("You need to enter the numbers as shown\n");
+	printf("It getting harder each time\n");
+	Sleep(2000);
+	system("@cls||clear");
 	do
 	{
 		node = makeNode();
 		head = addLast(head, node);
 		print_list(head);
+		Sleep(1500);
+		system("@cls||clear");
 		for (i = 0; i < getCount(head); i++)
 		{
 			userSelection = _getch(); // _getch capture keystroke on keyboard (without need for enter key) #include <conio.h>
@@ -30,10 +36,13 @@ int main() {
 				printf("corrent!\n");
 			}
 			else {
+				printf("Error!\n");
 				state = 0;
 				break;
 			}
 		}
+		Sleep(1500);
+		system("@cls||clear");
 	} while (state);
 	printf("You Loose!\n");
 	freeNode(head);
